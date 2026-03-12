d1eb8c0f20fabfcadc6f18131350bfc5ee65eb46
arjun-sapkota887 <asapkota@upei.ca>
2026-03-12 17:35:07 -0300

    feat(backend): add notes CRUD routes and set up nodemon dev/start scripts

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 using mern tech stack to create a note taking web app
+
+M- mongoDB: NoSQL database to store the data
+E - Express: web framework, we use framework so we don't have to write the code from scratch
+    A web framework is a ready to use toolbox for building web apps faster and more easily
+R - React: for frontend library
+N - Node JS: javascript runtime: allows us to run JS in server insted of on client
+
+CRUD routes - four basic database operations (Create, Read, Update, Delete)
+nodemon (a tool that auto-restarts the server when files change)
--- backend/server.js
+++ backend/server.js
@@ -4,13 +4,25 @@ import express from 'express';
 const app = express(); 
 
 app.get("/api/notes", (req, res) => {
-    res.status(200).send("you have 5 notes");
+    res.status(200).send("you have 15  notes");
 });
 
-app.get("/api/notes", (req, res) =>{
-    res.status(201).send("you have created a new note");
+app.get("/api/notes/note1", (req, res) => {
+    res.status(200).send("This is the content of your first note");
 });
 
+app.post("/api/notes", (req, res) => {
+    res.status(201).json({"message": "note created successfully"});
+});
+
+app.put('/api/notes/:id', (req, res) => {
+    res.status(200).json({"message": "note updated successfully"});
+});
+
+app.delete('/api/notes/:id', (req,res) => {
+    res.status(200).json({"message": "note deleted successfully"});
+});
+ 
 app.listen(5001, () => {
     console.log('Server started on port: 5001')
-})
+}) 
